ea27c7ab4e0dad538cad0d3565279c2357c99051
Louis Orleans <louis@orleans.io>
2016-08-23 20:06:10 -0700

    More gulp

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,7 @@ const Blessed = require('blessed');
 const Canvas = require('drawille');
 const Line = require('bresenham');
 
-
-const screen = Blessed.screen({
+const Term: any = Blessed.screen({
     smartCSR: true
 });
 
@@ -33,30 +32,30 @@ const box = Blessed.box({
 });
 
 // Quit on Escape, q, or Control-C.
-screen.key(['escape', 'q', 'C-c', ':q'], function(ch, key) {
+Term.key(['escape', 'q', 'C-c', ':q'], function(ch, key) {
     return process.exit(0);
 });
 
-screen.append(box);
+Term.append(box);
 
-screen.render();
+Term.render();
 
-var c = new Canvas(160, 160);
+let c = new Canvas(160, 160);
 
 function draw() {
     c.clear();
-    var t = new Date();
-    var sin = function(i, l) {
-        return Math.floor(Math.sin(i*2*Math.PI)*l+80);
+    let t = new Date();
+    let sin = function(i, l) {
+        return Math.floor(Math.sin(i * 2 * Math.PI) * l + 80);
     }, cos = function(i, l) {
-        return Math.floor(Math.cos(i*2*Math.PI)*l+80);
+        return Math.floor(Math.cos(i * 2 * Math.PI) * l + 80);
     };
-    Line(80, 80, sin(t.getHours()/24, 30), 160-cos(t.getHours()/24, 30), c.set.bind(c));
-    Line(80, 80, sin(t.getMinutes()/60, 50), 160-cos(t.getMinutes()/60, 50), c.set.bind(c));
-    Line(80, 80, sin(t.getSeconds()/60+(+t%1000)/60000, 75), 160-cos(t.getSeconds()/60+(+t%1000)/60000, 75), c.set.bind(c));
+    Line(80, 80, sin(t.getHours() / 24, 30), 160 - cos(t.getHours() / 24, 30), c.set.bind(c));
+    Line(80, 80, sin(t.getMinutes() / 60, 50), 160 - cos(t.getMinutes() / 60, 50), c.set.bind(c));
+    Line(80, 80, sin(t.getSeconds() / 60 + ( + t % 1000) / 60000, 75), 160 - cos(t.getSeconds() / 60 + ( + t % 1000) / 60000, 75), c.set.bind(c));
     //   process.stdout.write(c.frame());
     box.content = c.frame();
-    screen.render();
+    Term.render();
 }
 
-setInterval(draw, 1000/24);
+setInterval(draw, 1000 / 24);
